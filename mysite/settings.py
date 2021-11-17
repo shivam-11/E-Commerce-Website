a7300f4b1088289863ac59b3d77f205d7b3bd4ec
shivam-11 <48873715+shivam-11@users.noreply.github.com>
2021-11-17 11:34:08 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
