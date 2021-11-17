dd73ae622f8b92a9fd2e2982498f6a2d393bb781
shivam-11 <48873715+shivam-11@users.noreply.github.com>
2021-11-17 12:24:25 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
-    path('', views.index)
+    path('', views.index),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
